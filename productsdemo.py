e7b4e35accf864419d5e9d465fceabc485068626
shif99 <shifna.kc05@gmail.com>
2021-10-30 16:48:19 +0530

    python ci cd

---
+++ productsdemo.py
@@ -0,0 +1,22 @@
+from functools import reduce
+products = [
+{
+'productId':1,
+'productName':'Iphone',
+'price':135556.5
+},
+{
+'productId': 4,
+'productName':'Oneplus',
+'price':69999.5
+},
+{
+'productId':3,
+'productName':'SamsungFlip',
+'price':149999.5
+}
+
+]
+def myFunc():
+    return ""
+    print(reduce(lambda price1, price2 : price1+price2,(map(lambda p : p['price'],filter(lambda product: product['price'] > 10000.0, products))),10))
